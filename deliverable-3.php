5225843d869eeeec94e2a40a7bd6122353054744
ioleshko1 <117392319+ioleshko1@users.noreply.github.com>
2022-12-03 12:22:15 -0800

    deliverable-3.php

---
+++ deliverable-3.php
@@ -0,0 +1,49 @@
+<html>
+<body>
+
+ <form action= "deliverable-3-main-page.php" method="post">
+        <br>
+<input type="submit" value="View Table">
+</form>
+
+</body>
+
+<?php
+
+$servername = "ecs-pd-proj-db.ecs.csus.edu";
+$username = "CSC174043";
+$password = "Csc134_557929315";
+$mydb = "CSC174043";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$mydb);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//INSERT
+$sqlPlayerInsert = $conn->prepare("INSERT INTO PLAYER (userName, legendName)
+VALUES (?,?)");
+
+//PREPARED STATEMENTS
+$sqlPlayerInsert->bind_param("ss",$playername,$legendname);
+
+$playername = $_POST["username"];
+$legendname = $_POST["legendname"];
+
+
+// Check to see if the insert completed successfully
+if ($sqlPlayerInsert->execute() === TRUE) {
+  echo "New Legend record created successfully";
+} else {
+  echo "Error: " . $conn->error;
+}
+
+
+$sqlPlayerInsert->close();
+
+mysqli_close($conn);
+?>
+</html>
