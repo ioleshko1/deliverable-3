c324de0579aa1b4a0f00b4f4128b35cce71253d7
ioleshko1 <117392319+ioleshko1@users.noreply.github.com>
2022-12-03 12:51:20 -0800

    Update deliverable-3-main-page.php

--- deliverable-3-main-page.php
+++ deliverable-3-main-page.php
@@ -6,88 +6,142 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
+<!-- We are creating a body and changing the style of the tables, th, and td inside of the style tag -->
 <body>
 <style>
         table, th, td {
-        border: 1px solid black;
-        border-collapse: collapse;
+                border: 1px solid black;
+                border-collapse: collapse;
         }
 
         th, td {
-        padding: 15px
+                padding: 15px
         }
 </style>
 
 
+<!-- Here we are inserting some text to be displayed so the user can see what kind of data we want to be inputted -->
 We are inserting data into the PLAYER table which has two columns known as userName and legendName.
 <br>These are both varchar (50) fields which are meant to have strings inserted into them. Also userName is
 <br>the primary key of the table so it cannot be left blank.  If it is left blank the page will not submit
 <br>until it is filled.
 
+
+<!-- Added several breaks in here for neatness -->
 <br>
 <br>
 <br>
 
+
+<!-- Added a header to showcase that these is where the user can add Players -->
 <h2>Add Players</h2>
 
+
+<!-- Here we have a form available with the User Name, Legend Name, and the submit button along with a POST method to lead to the next page -->
 <form action="deliverable-3.php" method="post">
-Username: <input type="text" name="username" required><br>
-Legend name: <input type="text" name="legendname"><br>
-<input type="submit">
+        Username: <input type="text" name="username" required><br>
+        Legend name: <input type="text" name="legendname"><br>
+        <input type="submit">
 </form>
 
+
+<!-- Added several breaks in here for neatness -->
+<br>
 <br>
 <br>
 
+
+<!-- Added a header to showcase that this is where the data from the Player Table is being showcased -->
 <h2>Player Table</h2>
 
+
+<!-- Here we start the php command -->
 <?php
 
-$servername = "ecs-pd-proj-db.ecs.csus.edu";
-$username = "CSC174043";
-$password = "Csc134_557929315";
-$mydb = "CSC174043";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password,$mydb);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+        // First we create several variables to keep track of the database information
+        $servername = "ecs-pd-proj-db.ecs.csus.edu";
+        $username = "CSC174043";
+        $password = "Csc134_557929315";
+        $mydb = "CSC174043";
 
 
-//SELECT
-$sql = "SELECT username, legendName FROM PLAYER";
-$result = $conn->query($sql);
+        // Create a connection to the database using the variables that we created
+        $conn = new mysqli($servername, $username, $password,$mydb);
 
-if ($result->num_rows > 0) {
 
-?>
-<table>
-<tr>
-<td>User Name</td>
-<td>Legend Name</td>
-</tr>
+        // Check the connection to confirm that it is connecting properly
+        if ($conn->connect_error) {
 
-<?php
-    while($row = $result->fetch_assoc()) {
+                // If the coneection does not connect properly then kill the program and display the connect error
+                die("Connection failed: " . $conn->connect_error);
 
-echo " <tr>
-        <td>".$row["username"]."</td>
-        <td>".$row["legendName"]."</td>
-        </tr>";
         }
-} else {
-echo "0 results";
 
-}
 
-?>
-</table>
+        // Here we are creating several variable to keep track of the SQL query and the result that it is getting
+        $sql = "SELECT username, legendName FROM PLAYER";
+        $result = $conn->query($sql);
+
+
+        // Here we are checking to see if there are more than one row that has been returned from the query
+        if ($result->num_rows > 0) {
+
+?> <!-- End the php commands -->
+
+
+<!-- Here we are creating a table structure so we can insert the data that we are getting from the database in a clean fashion -->
+<table>
+        <tr>
+                <!-- Add a column name to the two columns that we will be using -->
+                <td>User Name</td>
+                <td>Legend Name</td>
+        </tr>
+
+
+<!-- Start up the PHP commands again -->
 <?php
-mysqli_close($conn);
-?>
 
+                // While we have more row that we can read through from the SQL query we keep reading
+                while($row = $result->fetch_assoc()) {
+
+
+                        // Here we are echoing all of the rows of data using the table format
+                        echo "<tr>
+                                     <td>".$row["username"]."</td>
+                                     <td>".$row["legendName"]."</td>
+                              </tr>";
+
+                } // End of the while loop
+
+        } // End of the if statement where we have more than one row from the SELECT query
+
+
+        // If we did not have any rows returned then we just output the fact that we have 0 results
+        else {
+                echo "0 results";
+        }
+
+?> <!-- End the php commands -->
+
+
+<!-- Here we are ending the table of the results that we are getting from the SELECT query -->
+</table>
+
+
+<!-- Start up the PHP commands again -->
+<?php
+
+        // Close the database connection to not waste resources
+        mysqli_close($conn);
+
+?> <!-- End the php commands -->
+
+
+<!-- End of the html body -->
 </body>
+
+
+<!-- End of the html -->
 </html>
